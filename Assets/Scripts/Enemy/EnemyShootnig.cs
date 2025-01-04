f526c1ba5f6018525f6977ca7bd12e16ba94de5f
RamiAlmuhana <ramealmuhana@hotmail.com>
2025-01-04 18:03:20 +0100

    1. enemy aangemaakt die op je schiet wanneer je in zijn range komt

---
+++ Assets/Scripts/Enemy/EnemyShootnig.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPosition;
+    private GameObject player;
+    
+    private float timer;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        
+        
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+        if (distance < 10)
+        {
+            FacePlayer();
+            
+            timer += Time.deltaTime;
+            
+            if (timer > 1)
+            {
+                timer = 0;
+                Shoot();
+            }
+        }
+    }
+    
+    private void FacePlayer()
+    {
+        // Bereken de richting naar de speler
+        Vector3 direction = player.transform.position - transform.position;
+
+        // Als de speler aan de rechterkant is, kijk naar rechts, anders naar links
+        if (direction.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Normale schaal voor rechts
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Gespiegeld voor links
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, bulletPosition.position, Quaternion.identity);
+    }
+}
