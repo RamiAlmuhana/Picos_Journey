c7dbc9a54cfb862d4cba4173a03515e2b1844dc2
mathieuvb <mathieu.van.beek@gmail.com>
2025-01-07 16:17:31 +0100

    Player jump en damage geluid toegevoegd

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,29 +1,83 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public float maxHealth;
     private HealthBarController healthBarController;
+    private PlayerMovement playerMovement;
+
+    [SerializeField]
+    private AudioClip deathSound;
+
+    private AudioSource audioSource;
+    private bool isDead = false;
 
     void Start()
     {
         maxHealth = health;
         healthBarController = GetComponentInChildren<HealthBarController>();
+        playerMovement = GetComponent<PlayerMovement>();
+        audioSource = GetComponent<AudioSource>(); 
     }
-    
+
     void Update()
     {
         if (healthBarController != null)
         {
             healthBarController.UpdateHealthBar(health, maxHealth);
         }
-        if (health <= 0)
+
+        if (health <= 0 && !isDead)
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene(SceneManager.loadedSceneCount);
+            isDead = true;
+            StartCoroutine(HandleDeath()); 
+        }
+    }
+
+    private IEnumerator HandleDeath()
+    {
+        PlayDeathSound();
+        DisablePlayerMovement();
+        HidePlayerAndHealthBar();
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void PlayDeathSound()
+    {
+        if (deathSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(deathSound);
+        }
+    }
+
+    private void HidePlayerAndHealthBar()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.enabled = false;
+        }
+
+        if (healthBarController != null)
+        {
+            var healthBarSpriteRenderer = healthBarController.spriteRenderer;
+            if (healthBarSpriteRenderer != null)
+            {
+                healthBarSpriteRenderer.enabled = false;
+            }
+        }
+
+    }
+
+    private void DisablePlayerMovement()
+    {
+        if (playerMovement != null)
+        {
+            playerMovement.canMove = false;
         }
     }
 }
