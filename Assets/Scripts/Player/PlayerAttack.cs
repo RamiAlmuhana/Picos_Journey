c17f1cf51431261806f852643855b861b3ec2109
mathieuvb <mathieu.van.beek@gmail.com>
2025-01-10 14:52:56 +0100

    Wapen geluiden toegevoegd

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // [SerializeField] private float attackCooldown;
+    // Attack variables
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject akBulletPrefab;
     [SerializeField] private GameObject pistolBulletPrefab;
     [SerializeField] private float pistolCooldown = 0.5f;
     [SerializeField] private float akCooldown = 0.25f;
+
+    // Sound effect variables
+    [SerializeField] private AudioClip akSoundEffect;
+    [SerializeField] private AudioClip pistolSoundEffect;
+    private AudioSource audioSource;
 
     private Animator anim;
     private PlayerMovement playerMovement;
@@ -17,6 +22,12 @@ public class PlayerAttack : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource component missing on PlayerAttack GameObject!");
+        }
     }
 
     private void Update()
@@ -34,36 +45,48 @@ public class PlayerAttack : MonoBehaviour
         {
             return;
         }
-        
+
         if (playerMovement.hasPistol && (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)) && cooldownTimer > pistolCooldown)
         {
             AttackPistol();
         }
         else if (playerMovement.hasAK && (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)) && cooldownTimer > akCooldown)
         {
-            Attack();
+            AttackAK();
         }
 
         cooldownTimer += Time.deltaTime;
     }
 
-    private void Attack()
+    private void AttackAK()
     {
         anim.SetTrigger("attack");
         cooldownTimer = 0;
 
+        // Play AK sound effect
+        if (audioSource != null && akSoundEffect != null)
+        {
+            audioSource.PlayOneShot(akSoundEffect);
+        }
+
+        // Spawn AK bullet
         GameObject bullet = Instantiate(akBulletPrefab, firePoint.position, Quaternion.identity);
         bullet.GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
-    
+
     private void AttackPistol()
     {
         anim.SetTrigger("attackPistol");
         cooldownTimer = 0;
 
+        // Play Pistol sound effect
+        if (audioSource != null && pistolSoundEffect != null)
+        {
+            audioSource.PlayOneShot(pistolSoundEffect);
+        }
+
+        // Spawn Pistol bullet
         GameObject bullet = Instantiate(pistolBulletPrefab, firePoint.position, Quaternion.identity);
         bullet.GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
-
-
 }
