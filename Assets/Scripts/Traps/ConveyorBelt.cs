554ce76da5f75656f25fdaf9e2eafb06168a5b4a
s1148541 <s1148541@student.hsleiden.nl>
2025-01-14 18:59:13 +0100

    Map updated with more conveyors

--- Assets/Scripts/Traps/ConveyorBelt.cs
+++ Assets/Scripts/Traps/ConveyorBelt.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     [SerializeField]
-    private float pushForce = 100f; // De kracht waarmee de speler wordt geduwd
+    private float pushForce = 100f;
     [SerializeField]
-    private bool pushLeft = true; // Richting van de duw, links of rechts
+    private bool pushLeft = true;
 
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -14,10 +14,8 @@ public class ConveyorBelt : MonoBehaviour
             Rigidbody2D playerBody = collision.gameObject.GetComponent<Rigidbody2D>();
             if (playerBody != null)
             {
-                // Bereken de kracht richting (links of rechts)
                 Vector2 forceDirection = pushLeft ? Vector2.left : Vector2.right;
 
-                // Pas de kracht toe op de speler
                 playerBody.AddForce(forceDirection * pushForce, ForceMode2D.Force);
             }
         }
