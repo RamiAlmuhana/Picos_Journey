38562057d356ab54931c8f425691c1e7d28a45f2
RamiAlmuhana <ramealmuhana@hotmail.com>
2025-01-17 16:14:22 +0100

    Als speler kan je nu Items oppakken net als water druppels of seed enzo

---
+++ Assets/Scripts/Item_pick-up/ItemPickUp.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    [SerializeField] private AudioClip collectSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collectSound != null)
+            {
+                PlaySoundAndDestroy(collectSound);
+            }
+
+            ItemManager.Instance.CollectItem();
+            Destroy(gameObject);
+        }
+    }
+
+    private void PlaySoundAndDestroy(AudioClip clip)
+    {
+        GameObject tempAudio = new GameObject("TempAudio");
+        AudioSource audioSource = tempAudio.AddComponent<AudioSource>();
+        
+        audioSource.clip = clip;
+        audioSource.Play();
+        
+        Destroy(tempAudio, clip.length);
+    }
+}
