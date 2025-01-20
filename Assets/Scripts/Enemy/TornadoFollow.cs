ede5958ce54373c81790f7ce2b6c0c55e2928ffa
RamiAlmuhana <ramealmuhana@hotmail.com>
2025-01-20 23:34:29 +0100

    1. Level 3 zo goed als af 2. traps toegevoegd 3. dialogues toegevoegd 4. items toegevoegd toegevoegd die de speler kan oppakken en uiteindelijk in de prullenbak kan gooien

--- Assets/Scripts/Enemy/TornadoFollow.cs
+++ Assets/Scripts/Enemy/TornadoFollow.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class TornadoFollow : MonoBehaviour
 {
-    public float speed = 2f; // Snelheid waarmee de tornado beweegt
-    public float damage = 10f; // Schade die de tornado toebrengt
-    private Transform player; // Referentie naar de speler
+    public float speed = 2f;
+    public float damage = 10f;
+    private Transform player;
 
     void Start()
     {
-        // Zoek de speler op basis van de tag "Player"
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-        // Controleer of de speler bestaat
         if (player != null)
         {
-            // Bereken de horizontale richting naar de speler
             Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
         }
@@ -25,10 +22,8 @@ public class TornadoFollow : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Controleer of de tornado de speler raakt
         if (collision.CompareTag("Player"))
         {
-            // Verminder de gezondheid van de speler
             PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
